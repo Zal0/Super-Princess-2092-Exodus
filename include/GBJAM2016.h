aad5249b8ad034afbc537c80c5a22efdfa683903
Zalo <cebolleto@gmail.com>
2016-10-07 20:14:49 +0200

    created project (not even compiling yet)

---
+++ include/GBJAM2016.h
@@ -0,0 +1,19 @@
+#ifndef GBJAM2016_H
+#define GBJAM2016_H
+
+#include "main.h"
+
+typedef enum {
+	STATE_MENU,
+	STATE_GAME,
+
+	N_STATES
+} STATE;
+
+typedef enum {
+	SPRITE_PRINCESS,
+
+	N_SPRITE_TYPES
+} SPRITE_TYPE;
+
+#endif
---
+++ include/StateGame.h
@@ -0,0 +1,8 @@
+#ifndef STATEGAME_H
+#define STATEGAME_H
+
+#include "main.h"
+
+DECLARE_STATE(STATE_GAME);
+
+#endif
