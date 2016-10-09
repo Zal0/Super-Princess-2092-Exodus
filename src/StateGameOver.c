73ac43e79f8f6bba9aa651dd2da116891d0d34ee
Zalo <cebolleto@gmail.com>
2016-10-09 19:35:31 +0200

    gameover music

--- src/StateGameOver.c
+++ src/StateGameOver.c
@@ -10,14 +10,14 @@ UINT8 bank_STATE_GAMEOVER = 2;
 #include "../res/src/pressstarttileset.h"
 #include "../res/src/gameovertilemap.h"
 
-//extern const unsigned char * start_mod_Data[];
+extern const unsigned char* exo_gameover_mod_Data[];
 
 void Start_STATE_GAMEOVER() {
 	InitScrollTiles(0, 128, pressstarttileset, 3);
 	InitScroll(gameovertilemapWidth, gameovertilemapHeight, gameovertilemap, 0, 0, 3);
 	SHOW_BKG;
 
-	//PlayMusic(start_mod_Data, 2, 1);
+	PlayMusic(exo_gameover_mod_Data, 4, 0);
 }
 
 void Update_STATE_GAMEOVER() {
