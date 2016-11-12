0a5efbcdbd0d9bdbcbe3d4985a6bc1616063bd0a
Zalo <cebolleto@gmail.com>
2016-11-12 17:55:10 +0100

    fixed imposible to avoid mushrom

--- res/src/stage1_3.h
+++ res/src/stage1_3.h
@@ -8,7 +8,7 @@
    Section       : 
    Bank          : 0
    Map size      : 24 x 40
-   Tile set      : D:\GameBoyJam2016\res\stage1_bg.gbr
+   Tile set      : stage1_bg.gbr
    Plane count   : 1 plane (8 bits)
    Plane order   : Tiles are continues
    Tile offset   : 0
