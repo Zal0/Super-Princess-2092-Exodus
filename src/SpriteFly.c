f93b7b368fb405fef50736ebbb2430075cad0c12
Zalo <cebolleto@gmail.com>
2016-11-12 10:54:02 +0100

    totally rewriteen fly behaviour

--- src/SpriteFly.c
+++ src/SpriteFly.c
@@ -9,59 +9,54 @@ UINT8 bank_SPRITE_FLY = 2;
 
 struct FlyCustomData {
 	fixed vx, vy;
-	UINT16 h;
+	UINT16 tx, ty;
 };
 
 
 const UINT8 anim_fly[] = {2, 0, 1};
 
-#define DIST_ACTION 40
-#define DIST_COUNTER 40
+#define DIST_ACTION 20
+#define DIST_COUNTER 10
 #define ACCEL_OFFSET 4
-#define HEIGHT 30
+#define HEIGHT 50
 void Start_SPRITE_FLY(struct Sprite* sprite) {
 	struct FlyCustomData* data = (struct FlyCustomData*)sprite->custom_data;
 	
 	SetSpriteAnim(sprite, anim_fly, 33);
 	data->vx.w = 0;
 	data->vy.w = 0;
-	data->h = scroll_target->y - HEIGHT;
+	data->tx = 0;
 }
+
+#define DISTANCE(A, B) (U_LESS_THAN(A, B) ? (B - A) : (A - B))
 
 void Update_SPRITE_FLY() {
 	struct FlyCustomData* data = (struct FlyCustomData*)sprite_manager_current_sprite->custom_data;
 
-	if(sprite_manager_current_sprite->flags == 0) { //Moving Left
-		data->vx.w -= 100;
-		if(U_LESS_THAN(scroll_target->x, sprite_manager_current_sprite->x)) {
-			if(U_LESS_THAN(sprite_manager_current_sprite->x - scroll_target->x, scroll_target->y - sprite_manager_current_sprite->y)){
-				data->vx.w += (scroll_target->x - sprite_manager_current_sprite->x) << ACCEL_OFFSET;
-				data->vy.w += (scroll_target->y + 8 - sprite_manager_current_sprite->y) << ACCEL_OFFSET;
-				data->h = scroll_target->y - HEIGHT;
-			} 
-		} else {
-			if(U_LESS_THAN(sprite_manager_current_sprite->x + DIST_COUNTER, scroll_target->x)){
-				sprite_manager_current_sprite->flags = OAM_VERTICAL_FLAG;
-			} else {
-				data->vy.w += (data->h - sprite_manager_current_sprite->y) << ACCEL_OFFSET;
-			}
+	if(data->tx == 0) {
+		data->vx.w = (sprite_manager_current_sprite->flags == 0) ? (data->vx.w - 100) : (data->vx.w + 100);
+		if(U_LESS_THAN(DISTANCE(sprite_manager_current_sprite->x, scroll_target->x), DIST_ACTION)){
+			data->tx = scroll_target->x;
+			data->ty = scroll_target->y + 16;
 		} 
 	} else {
-		data->vx.w += 100;
-		if(U_LESS_THAN(sprite_manager_current_sprite->x, scroll_target->x)) {
-			if(U_LESS_THAN(scroll_target->x - sprite_manager_current_sprite->x, scroll_target->y - sprite_manager_current_sprite->y)){
-				data->vx.w += (scroll_target->x - sprite_manager_current_sprite->x) << ACCEL_OFFSET;
-				data->vy.w += (scroll_target->y + 8 - sprite_manager_current_sprite->y) << ACCEL_OFFSET;
-				data->h = scroll_target->y - HEIGHT;
-			} 
-		} else {
-			if(U_LESS_THAN(scroll_target->x, sprite_manager_current_sprite->x - DIST_COUNTER)){
-				sprite_manager_current_sprite->flags = 0;
-			} else {
-				data->vy.w += (data->h - sprite_manager_current_sprite->y) << ACCEL_OFFSET;
+		data->vx.w += (data->tx - sprite_manager_current_sprite->x) << ACCEL_OFFSET;
+		data->vy.w += (data->ty - sprite_manager_current_sprite->y) << ACCEL_OFFSET;
+		if(U_LESS_THAN(sprite_manager_current_sprite->y, data->ty)) {
+			//ATTACKING
+			if(U_LESS_THAN(DISTANCE(data->ty, sprite_manager_current_sprite->y), 8)){
+				data->tx = (sprite_manager_current_sprite->flags == 0) ? (data->tx - DIST_COUNTER) : (data->tx + DIST_COUNTER); 
+				data->ty = data->ty - HEIGHT;
 			}
-		} 
+		} else {
+			//CHARGING 
+			if(U_LESS_THAN(DISTANCE(data->ty, sprite_manager_current_sprite->y), 4)){
+				sprite_manager_current_sprite->flags = U_LESS_THAN(scroll_target->x, sprite_manager_current_sprite->x) ? 0 : OAM_VERTICAL_FLAG;
+				data->tx = 0;
+			}
+		}
 	}
+	
 
 	sprite_manager_current_sprite->x += (INT16)data->vx.b.h;
 	sprite_manager_current_sprite->y += (INT16)data->vy.b.h;
