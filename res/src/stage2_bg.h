b2a57ae6191063ea41353de533527dd8ab4ef3fc
Zalo <cebolleto@gmail.com>
2016-11-19 19:42:06 +0100

    stage2 moved to bank 5

--- res/src/stage2_bg.h
+++ res/src/stage2_bg.h
@@ -10,7 +10,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 54
+  Tiles                : 0 to 58
 
   Palette colors       : None.
   SGB Palette          : None.
