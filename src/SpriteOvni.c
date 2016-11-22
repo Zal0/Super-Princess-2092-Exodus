11ba8f24fff2d1d14de20d8e11312a32e814e5b4
Zalo <cebolleto@gmail.com>
2016-11-22 19:39:15 +0100

    ensuring the Ovni is visible before attacking

--- src/SpriteOvni.c
+++ src/SpriteOvni.c
@@ -15,11 +15,7 @@ struct OvniCustomData {
 void Start_SPRITE_OVNI(struct Sprite* sprite) {
 	struct OvniCustomData* data = (struct OvniCustomData*)sprite->custom_data;
 
-	if(U_LESS_THAN(scroll_target->x, sprite->x)) {
-		data->vx.w = -255;
-	} else {
-		data->vx.w = 255;
-	}
+	data->vx.w = 0;
 	data->tx.w = 0;
 	data->missile_launched = 0;
 }
@@ -27,16 +23,28 @@ void Start_SPRITE_OVNI(struct Sprite* sprite) {
 void Update_SPRITE_OVNI() {
 	struct OvniCustomData* data = (struct OvniCustomData*)sprite_manager_current_sprite->custom_data;
 	
-	data->tx.w += data->vx.w << (INT16)delta_time;
-	sprite_manager_current_sprite->x += (INT16)data->tx.b.h;
-	data->tx.b.h = 0;
+	if(data->vx.w == 0) {
+		if(U_LESS_THAN(sprite_manager_current_sprite->y, scroll_y)) {
+			sprite_manager_current_sprite->y ++;
+		} else {
+			if(U_LESS_THAN(scroll_target->x, sprite_manager_current_sprite->x)) {
+				data->vx.w = -255;
+			} else {
+				data->vx.w = 255;
+			}
+		}
+	} else {
+		data->tx.w += data->vx.w << (INT16)delta_time;
+		sprite_manager_current_sprite->x += (INT16)data->tx.b.h;
+		data->tx.b.h = 0;
 
-	if(data->missile_launched == 0) {
-		if( (U_LESS_THAN(data->vx.w, 0) && U_LESS_THAN(sprite_manager_current_sprite->x, scroll_target->x)) ||
-			  (U_LESS_THAN(0, data->vx.w) && U_LESS_THAN(scroll_target->x, sprite_manager_current_sprite->x)) 
-		) {
-			SpriteManagerAdd(SPRITE_MISSILE, sprite_manager_current_sprite->x, sprite_manager_current_sprite->y + 8);
-			data->missile_launched = 1;
+		if(data->missile_launched == 0) {
+			if( (U_LESS_THAN(data->vx.w, 0) && U_LESS_THAN(sprite_manager_current_sprite->x, scroll_target->x)) ||
+					(U_LESS_THAN(0, data->vx.w) && U_LESS_THAN(scroll_target->x, sprite_manager_current_sprite->x)) 
+			) {
+				SpriteManagerAdd(SPRITE_MISSILE, sprite_manager_current_sprite->x, sprite_manager_current_sprite->y + 8);
+				data->missile_launched = 1;
+			}
 		}
 	}
 }
