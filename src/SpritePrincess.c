2a57952ec780dc7dc7bec677b338d311642478b7
Zalo <cebolleto@gmail.com>
2016-12-06 13:36:54 +0100

    testing iteration macro

--- src/SpritePrincess.c
+++ src/SpritePrincess.c
@@ -280,8 +280,7 @@ void Update_SPRITE_PRINCESS() {
 #endif
 
 	//Check enemy collision
-	for(i = 0u; i != sprite_manager_updatables[0]; ++i) {
-		spr = sprite_manager_sprites[sprite_manager_updatables[i + 1u]];
+	SPRITEMANAGER_ITERATE(i, spr) {
 		if(spr->type == SPRITE_MUSHROOM || spr->type == SPRITE_ENEMY_BULLET || spr->type == SPRITE_CSHOOTER || spr->type == SPRITE_SHOOTER ||
 			 spr->type == SPRITE_FLY || spr->type == SPRITE_ROLLER || spr->type == SPRITE_OVNI || spr->type == SPRITE_MISSILE) {
 			if(CheckCollision(THIS, spr)) {
